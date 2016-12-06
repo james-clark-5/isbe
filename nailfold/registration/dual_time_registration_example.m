d208fc07bb3f0b941ea56504cade05b3a0a2181e
james-clark-5 <james.clark-5@student.manchester.ac.uk>
2016-12-06 11:59:24 +0000

    Directory changes
    - Set up directories

--- nailfold/registration/dual_time_registration_example.m
+++ nailfold/registration/dual_time_registration_example.m
@@ -1,12 +1,17 @@
 %Example script for preparing and the registering frames captured from the
 %dual camera system
 
-%Set folder containing all the frames
-frames_dir = 'C:\isbe\nailfold\data\dual_wavelength\550 nm t_compare_t0_t2\';
+% jj testing git
 
-%Make dirt image - I don't have frames for this, so just reusing the above.
+%jj tell the code where to look for called files
+addpath(genpath('C:\Users\MPhys2016\Documents\GitHub\isbe'));
+
+%Set folder containing all the frames
+frames_dir = 'C:\isbe\nailfold\camera_capture\dual_cameras\Finite microscope\Two Cameras\3-light\cuffing\16.11.22 - 530 550 nm - g1 1023, g2 260, exp 76.6 + dirt image\530 nm t_compare_t0_t2\';
+
+%Make dirt image
 %You want to use the (out-of-focus) frames you captured especially for this
-dirty_dir = 'C:\isbe\nailfold\data\dual_wavelength\550 nm t_compare_t0_t2\'; %
+dirty_dir = 'C:\isbe\nailfold\camera_capture\dual_cameras\Finite microscope\Two Cameras\3-light\cuffing\16.11.22 - 530 550 nm - g1 1023, g2 260, exp 76.6 + dirt image\530 nm dirt image\'; %
 [dirt_image] = make_dirt_image(dirty_dir);
 figure; imgray(dirt_image); colorbar;
 
